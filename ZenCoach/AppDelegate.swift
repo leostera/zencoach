b2d29168a18d09d87792653c891536d6ce074af2
Leandro Ostera <leandro@ostera.io>
2016-08-13 13:32:25 +0200

    Unpack data and track it nicely

--- ZenCoach/AppDelegate.swift
+++ ZenCoach/AppDelegate.swift
@@ -7,34 +7,54 @@
 //
 
 import Cocoa
-import IOKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  var bar = NSStatusBar.systemStatusBar()
+  var item : NSStatusItem = NSStatusItem()
+  var user : String?
+  
+  override init() {
+    let userDefaults = NSUserDefaults.standardUserDefaults()
     
-    var bar = NSStatusBar.systemStatusBar()
-    var item : NSStatusItem = NSStatusItem()
-
-    override func awakeFromNib() {
-        self.item = self.bar.statusItemWithLength(-1)
-        self.item.enabled = false
-        self.item.title = "ZenCoach"
+    if userDefaults.objectForKey("UUID") == nil {
+      let UUID = NSUUID().UUIDString
+      userDefaults.setObject(UUID, forKey: "UUID")
+      userDefaults.synchronize()
     }
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        NSWorkspace
-            .sharedWorkspace()
-            .notificationCenter
-            .addObserverForName(NSWorkspaceDidActivateApplicationNotification,
-                            object: nil,
-                            queue: NSOperationQueue.mainQueue()
-            ) { x in print(x.userInfo.bundleIdentifier!) }
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-
+    self.user = userDefaults.stringForKey("UUID")
+    super.init()
+  }
+  
+  override func awakeFromNib() {
+    self.item = self.bar.statusItemWithLength(-1)
+    self.item.enabled = false
+    self.item.title = "ZenCoach"
+  }
+  
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    NSWorkspace
+      .sharedWorkspace()
+      .notificationCenter
+      .addObserverForName(NSWorkspaceDidActivateApplicationNotification,
+                          object: nil,
+                          queue: NSOperationQueue.mainQueue()
+      ) { x in self.track(self.user!, data: self.unpackBundleIdentifier(x.userInfo!)!)}
+  }
+  
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+  
+  func unpackBundleIdentifier(data: NSObject) -> String? {
+    return data.valueForKey("NSWorkspaceApplicationKey")!.bundleIdentifier
+  }
+  
+  func track(user: String, data: NSObject) {
+    print(user, data)
+  }
+  
+  
 }
 
