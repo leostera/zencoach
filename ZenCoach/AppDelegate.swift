958c301313b5eb523f4e3ec7999be6a45301a69e
Leandro Ostera <leandro@ostera.io>
2016-08-13 12:25:05 +0200

    Initial Commit

---
+++ ZenCoach/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ZenCoach
+//
+//  Created by Leandro Ostera on 8/13/16.
+//  Copyright © 2016 ostera.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
